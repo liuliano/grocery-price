eded016ba7d9ffa5058439a5e6d1f6b7d0fedce3
liuliano <iulianolawrence@yahoo.com>
2026-07-11 12:27:43 -0400

    Add grocery app configuration module

---
+++ js/config.js
@@ -0,0 +1,13 @@
+export const APP_CONFIG = {
+  zipCode: "33579",
+  stores: ["Walmart", "Publix", "Aldi", "Sam's Club"],
+  workflowUrl: "https://github.com/liuliano/grocery-price/actions/workflows/refresh-prices.yml",
+  storageKey: "grocery-price-shopping-list-33579",
+  defaultItems: [
+    "whole milk 1 gallon",
+    "large eggs 12 count",
+    "boneless chicken breast",
+    "bananas",
+    "sandwich bread"
+  ]
+};
